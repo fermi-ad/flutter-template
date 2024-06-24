3ef869675e7a0d331498345da0d906c47ed86a96
Beau Harrison <beau@fnal.gov>
2024-06-24 16:04:45 -0500

    Use the StandardApp from the flutter_core.
    Signed-off-by: Beau Harrison <beau@fnal.gov>

--- lib/main.dart
+++ lib/main.dart
@@ -13,52 +13,26 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return StandardApp(
       title: _title,
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      appBar: AppBar(
+        title: const Text(_title),
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          // TRY THIS: Try changing the color here to a specific color (to
-          // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-          // change color while the other colors stay the same.
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
-          title: const Text(_title),
-        ),
-        body: Center(
-          // Center is a layout widget. It takes a single child and positions it
-          // in the middle of the parent.
-          child: StreamBuilder(
-            stream: service.monitorDevices(["G:SCTIME@P,15H"]),
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return Text('Supercycle time: ${snapshot.data!.value!.toStringAsFixed(2)}');
-              }
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: StreamBuilder(
+          stream: service.monitorDevices(["G:SCTIME@P,15H"]),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Text(
+                  'Supercycle time: ${snapshot.data!.value!.toStringAsFixed(2)}');
+            }
 
-              return const Text('Loading...');
-            },
-          ),
-        ), // This trailing comma makes auto-formatting nicer for build methods.
-      ),
+            return const Text('Loading...');
+          },
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
