e1946a17aeb2a912bec5c7d0c3658088d3d7dafe
rnethi1701 <rnethi@fnal.gov>
2025-09-11 16:01:47 -0500

    Update sample_widget_test.dart

--- test/widget/sample_widget_test.dart
+++ test/widget/sample_widget_test.dart
@@ -1,1 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('renders a text widget', (tester) async {
+    await tester.pumpWidget(const MaterialApp(home: Text('Flutter Template Repo')));
+    expect(find.text('Flutter Template Repo'), findsOneWidget);
+  });
+}
 
