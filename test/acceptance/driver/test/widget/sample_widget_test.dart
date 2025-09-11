24b07212e01f8c35280f3f2b88dc87bf6016f954
rnethi1701 <rnethi@fnal.gov>
2025-09-11 15:53:20 -0500

    Delete test/acceptance/driver/test/widget/sample_widget_test.dart

--- test/acceptance/driver/test/widget/sample_widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  testWidgets('renders a text widget', (tester) async {
-    await tester.pumpWidget(const MaterialApp(home: Text('Hello')));
-    expect(find.text('Hello'), findsOneWidget);
-  });
-}
